67a402e0295189dc0f71f2b99b38c5d1764cae30
Tad-Min <nguyenvanthanhdat44@gmail.com>
2026-01-08 01:43:36 +0700

    Update IChatRepository.cs

---
+++ OverLut/Models/Repositories/IChatRepository.cs
@@ -0,0 +1,34 @@
+﻿using OverLut.Models.BusinessObjects;
+using OverLut.Models.DTOs;
+
+namespace OverLut.Models.Repositories
+{
+    public interface IChatRepository
+    {
+        #region Channel methods
+        Task<bool> CreateChannelAsync();
+        Task<IEnumerable<ChannelDTO>> GetAllChannelByUserIDAsync(Guid userID);
+        Task<IEnumerable<ChannelDTO>> GetAllChannelByChannelName(string ChannelName);
+        Task<ChannelDTO> EditChannelAsync();
+        Task<bool> UpdateChannelAsync();
+        Task<bool> DeleteChannelAsync();
+        #endregion
+
+        #region member methods
+        Task<bool> AddMemberToChannelAsync();
+        Task<bool> RemoveMemberFromChannelAsync();
+        Task<bool> UdateMemberNickName();
+        #endregion
+
+        #region Message methods
+        Task<bool> SendMessageAsync();
+        Task<IEnumerable<MessageDTO>> GetMessagesAsync();
+        Task<bool> DeleteMessageAsync();
+        #endregion
+        #region Attachment methods
+        #endregion
+        #region ReadReceipt methods
+        #endregion
+
+    }
+}
